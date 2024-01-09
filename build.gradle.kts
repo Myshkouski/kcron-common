5d1dc92aee8ced58117b32af9cdb929d7f1d10c8
Alexei Myshkouski <alexeimyshkouski@gmail.com>
2024-01-09 11:17:42 +0300

    Revert "implement signing skip for dev/test environments"
    This reverts commit 5f898b59f73b728192c2635f54e81e858d09c8b6.

--- build.gradle.kts
+++ build.gradle.kts
@@ -119,14 +119,6 @@ signing {
     sign(publishing.publications)
 }
 
-tasks.withType<Sign>().configureEach {
-    val propName = "signing.skip"
-    onlyIf("$propName is set") {
-        val skipSigning = project.extra[propName]?.toString()?.toBooleanStrict() ?: false
-        !skipSigning
-    }
-}
-
 tasks.withType<AbstractPublishToMaven>().configureEach {
     val signingTasks = tasks.withType<Sign>()
     mustRunAfter(signingTasks)
